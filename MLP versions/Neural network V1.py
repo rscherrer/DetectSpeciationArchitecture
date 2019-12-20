02e66297e9c562a11a433c7430ab756a298b35d1
Lexxie32 <56342428+Lexxie32@users.noreply.github.com>
2019-12-20 10:14:23 +0100

    Added node loop and export to csv

--- MLP versions/Neural network V1.py
+++ MLP versions/Neural network V1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+import pandas
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"   
@@ -18,9 +19,6 @@ all_data = np.frombuffer(data, np.float64)
 train_data = all_data[0:ntrainsims*ngenes]
 test_data = all_data[ntrainsims*ngenes:79600]
 
-print("all data", len(all_data))
-print("train data", len(train_data))
-print("test data", len(test_data))
 
 # read file for complex data
 with open(filenameComplex, "rb") as binary_file:
@@ -29,9 +27,6 @@ all_data2 =  np.frombuffer(data, np.float64)
 train_temp = all_data2[0:ntrainsims*ngenes]
 test_temp = all_data2[ntrainsims*ngenes:79600]
 
-print("all data 2", len(all_data2))
-print("train temp", len(train_temp))
-print("test temp", len(test_temp))
 
 # append complex data to simple
 train_data = np.append(train_data, train_temp)
@@ -44,22 +39,33 @@ test_data = np.reshape(test_data, (ntestsims*2,ngenes))
 train_labels = np.full((ntrainsims * 2,2), 0)
 train_labels[:ntrainsims , 0].fill(1)
 train_labels[ntrainsims: , 1].fill(1)
-print(train_labels)
+
 test_labels = np.full((ntestsims * 2, 2), 0)
 test_labels[:ntestsims , 0].fill(1)
 test_labels[:ntestsims , 1].fill(1)
 
-model = Sequential()
-model.add(Dense(64, input_dim=400, activation='relu'))
-model.add(Dense(2, activation='softmax'))
+data = []
+test_nodes = [2,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380,400]
+for n in range(2,401):
+    print(n)
+    scores = np.empty(10)
+    for m in range(10):
+        model = Sequential()
+        model.add(Dense(n, input_dim=400, activation='relu'))
+        model.add(Dense(2, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
+        model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(train_data,train_labels,
-          epochs=30,
-          batch_size=10)
+        model.fit(train_data,train_labels,
+          epochs=20,
+          batch_size=20, verbose=0)
 
-score = model.evaluate(test_data, test_labels, batch_size=20)
-print(score)
+        (loss,accuracy) = model.evaluate(test_data, test_labels, batch_size=10)
+        scores[m] = accuracy
+
+    data.append([n,scores.mean(),np.percentile(scores,95)])
+
+df = pandas.DataFrame(data, columns = ['Nodes', 'Accuracy', 'Percentile']) 
+df.to_csv('V1.csv')
